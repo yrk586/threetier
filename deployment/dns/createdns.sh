ef016af8bc576f5c279b7959b15239237c8e1f95
EC2 Default User <ec2-user@ip-10-0-1-214.ec2.internal>
2018-08-19 09:23:24 +0000

    Made changes

---
+++ deployment/dns/createdns.sh
@@ -0,0 +1,4 @@
+sleep 30
+LBNAME=`/usr/local/bin/kubectl describe service frontend|grep Ingress|awk '{print $NF}'`
+sed -i "s/FELBNAME/$LBNAME/g" dns.json
+aws route53 change-resource-record-sets --hosted-zone-id Z1LW9HTX8DFQS --change-batch file://dns.json
